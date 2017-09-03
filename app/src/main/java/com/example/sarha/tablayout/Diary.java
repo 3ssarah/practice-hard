bccce21c77c763be7ab149b6df418a9fbc5ae0cc
Sarah <ssarha0731@gmail.com>
2017-09-03 12:59:53 +0900

    first

---
+++ app/src/main/java/com/example/sarha/tablayout/Diary.java
@@ -0,0 +1,45 @@
+package com.example.sarha.tablayout;
+
+/**
+ * Created by SaRha on 2017-09-03.
+ */
+
+public class Diary {
+
+    private int number;
+    private String date;
+    private String contents;
+    private int color;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+}
